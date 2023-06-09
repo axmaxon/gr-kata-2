c41ea5f892dd25852165ca665cab4eb918cf47eb
maxim <axmaxon@gmail.com>
2023-06-09 09:54:16 +0500

    Refactor logic of items creation and quality update

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,96 +1,100 @@
-class GildedRose
-  ITEM_HANDLERS = {
-    'Aged Brie' => :update_aged_brie,
-    'Backstage passes to a TAFKAL80ETC concert' => :update_backstage_pass,
-    'Sulfuras, Hand of Ragnaros' => :update_sulfuras,
-    'Conjured Mana Cake' => :update_conjured
-  }
+module GildedRose
+  class Item
+    attr_accessor :name, :sell_in, :quality
 
-  def initialize(items)
-    @items = items
+    def initialize(name, sell_in, quality)
+      @name = name
+      @sell_in = sell_in
+      @quality = quality
+    end
+
+    def to_s()
+      "#{@name}, #{@sell_in}, #{@quality}"
+    end
   end
 
-  def update_quality
-    @items.each do |item|
-      item_handler = ITEM_HANDLERS[item.name] || :update_regular_item
-      send(item_handler, item)
+  class RegularItem < Item
+    def update_quality
+      if @quality > 0
+        @sell_in > 0 ? self.decrease_quality(1) : self.decrease_quality(2)
+      end
+
+      decrease_sell_in
     end
+  end
+
+  class AgedBrie < Item
+    def update_quality
+      if @quality < 50
+        @sell_in > 0 ? increase_quality(1) : increase_quality(2)
+      end
+
+      decrease_sell_in
+    end
+  end
+
+  class Backstage < Item
+    def update_quality
+      if @quality < 50
+        case @sell_in
+        when (11..)
+          increase_quality(1)
+        when (6..10)
+          increase_quality(2)
+        when (1..5)
+          increase_quality(3)
+        end
+      end
+
+      @quality = 0 if @sell_in <= 0
+
+      decrease_sell_in
+    end
+  end
+
+  # Является легендарным товаром с неизменяемыми свойствами
+  class Sulfuras < Item
+    def update_quality
+    end
+  end
+
+  class Conjured < Item
+    def update_quality
+      if @quality > 0
+        @sell_in > 0 ? decrease_quality(2) : decrease_quality(4)
+      end
+
+      decrease_sell_in
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Aged Brie' => AgedBrie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Sulfuras, Hand of Ragnaros' => Sulfuras,
+    'Conjured Mana Cake' => Conjured
+  }
+
+  def self.for(name, sell_in, quality)
+    (SPECIALIZED_CLASSES[name] || RegularItem).new(name, sell_in, quality)
   end
 
   private
 
-  def update_regular_item(item)
-    if item.quality > 0
-      item.sell_in > 0 ? decrease_quality(item, 1) : decrease_quality(item, 2)
-    end
-
-    decrease_sell_in(item)
-  end
-
-  def update_aged_brie(item)
-    if item.quality < 50
-      item.sell_in > 0 ? increase_quality(item, 1) : increase_quality(item, 2)
-    end
-
-    decrease_sell_in(item)
-  end
-
-  def update_backstage_pass(item)
-    if item.quality < 50
-      case item.sell_in
-      when (11..)
-        increase_quality(item, 1)
-      when (6..10)
-        increase_quality(item, 2)
-      when (1..5)
-        increase_quality(item, 3)
-      end
-    end
-
-    item.quality = 0 if (..0).include?(item.sell_in)
-
-    decrease_sell_in(item)
-  end
-
-  def update_sulfuras(_)
-    # Товар имеет неизменяемые свойства
-  end
-
-  def update_conjured(item)
-    if item.quality > 0
-      item.sell_in > 0 ? decrease_quality(item, 2) : decrease_quality(item, 4)
-    end
-
-    decrease_sell_in(item)
-  end
-
-  def increase_quality(item, value)
+  def increase_quality(value)
     value.times do
-      item.quality += 1 if item.quality < 50
+      @quality += 1 if @quality < 50
     end
   end
 
-  def decrease_quality(item, value)
+  def decrease_quality(value)
     value.times do
-      item.quality -= 1 if item.quality > 0
+      @quality -= 1 if @quality > 0
     end
   end
 
-  def decrease_sell_in(item)
-    item.sell_in -= 1
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+  def decrease_sell_in
+    @sell_in -= 1
   end
 end
